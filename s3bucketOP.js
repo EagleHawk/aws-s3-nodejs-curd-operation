0848b99b5cbe09d2f6beb4238fe2221d8167b65b
Eagle Hawk <eagle.soaringheights@gmail.com>
2018-05-03 18:58:51 +0530

    Help Added

--- s3bucketOP.js
+++ s3bucketOP.js
@@ -6,8 +6,30 @@
 // Load the SDK and UUID
 const yargs = require('yargs');
 const awsWorker = require('./workingforce/worker');
-
-const argv = yargs.argv;
+const bucketNameOption = {
+	describe: 'Specifies the bucket name',
+	demand: true,
+	alias: 'bn'
+};
+const bucketRegionOption = {
+	describe: 'Specifies the bucket region, if not specified then default region will be us-west-1',
+	demand: false,
+	alias: 'br'
+}
+const argv = yargs
+	.command('addbucket', 'Add a new bucket.', {
+		bucketname: {
+			describe: 'Specifies the bucket name, if not specified then name will be like node-sdk-00-<<random id>>',
+			demand: false,
+			alias: 'bn'
+		},
+		bucketregion: bucketRegionOption
+	})
+	.command('listbucket', 'Lists all the buckets created by user.')
+	.command('deletebucket', 'Deletes a specified bucket.', {
+		bucketname: bucketNameOption
+	})
+	.argv;
 let command = argv._[0];
 // console.log(`fire argv ${JSON.stringify(argv)} `);
 // console.log(`fire command ${command} `);
