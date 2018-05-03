023611bb7b9d8b8c73f6d19f6136f81cd2fe3f25
Eagle Hawk <eagle.soaringheights@gmail.com>
2018-05-03 18:25:51 +0530

    Initial Commit; Using AWS SDK S3 for CURD operations

---
+++ s3bucketOP.js
@@ -0,0 +1,67 @@
+/*
+ * Modified by : Devendra Walanj
+ * Purpose : Performing a CURD operation on the AWS S3 
+**/
+
+// Load the SDK and UUID
+const yargs = require('yargs');
+const awsWorker = require('./workingforce/worker');
+
+const argv = yargs.argv;
+let command = argv._[0];
+// console.log(`fire argv ${JSON.stringify(argv)} `);
+// console.log(`fire command ${command} `);
+
+
+// var bucketName = 'node-sdk-sample-' + uuid.v4();
+// var keyName = 'hello_world.txt';
+
+if (command === 'listbucket') {
+	awsWorker.listBuckets() ;
+
+} else if (command === 'addbucket') {
+	// Create a bucket and upload something into it
+	let bucketName = '', bucketRegion = '' ;
+	if (argv.hasOwnProperty('bucketname')) 
+		bucketName = argv.bucketname.trim() ;
+
+	if (argv.hasOwnProperty('bucketregion'))
+		bucketRegion = argv.bucketregion;
+		
+	awsWorker.createBucket( {bucketName:bucketName, bucketRegion:bucketRegion} ) ;
+	
+} else if (command === 'deletebucket') {
+	let bucketName = '', bucketRegion = '';
+	if (argv.hasOwnProperty('bucketname'))
+		bucketName = argv.bucketname.trim();
+	
+	if (bucketName === '') {
+		return console.log(`Specify the Bucket`);		
+	}
+	awsWorker.deleteOBucket({ bucketName: bucketName});
+	
+
+} else if (command === 'addobject') {
+
+} else if (command === 'deleteobject') {
+
+}
+
+
+// s3.createBucket({Bucket: bucketName}, function() {
+//   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+//   s3.putObject(params, function(err, data) {
+//     if (err)
+//       console.log(err)
+//     else
+//       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+//   });
+// });
+
+// s3.deleteBucket({Bucket: bucketName}, (err, data) => {
+//   if (err) {
+//       return console.log(`Error Occured on DeleteBucket -> ${err} `);      
+//   } 
+//   console.log(`S3 bucket ${bucketName} deleted successfully. `);
+
+// }) ;
---
+++ workingforce/worker.js
@@ -0,0 +1,90 @@
+const uuid = require('uuid');
+var AWS = require('aws-sdk');
+const _REGION = 'us-west-1';
+// const _REGION = 's3.us-west-1.amazonaws.com';
+// const _REGION = 's3-eu-west-1.amazonaws.com';
+// Create an S3 client
+let s3 = new AWS.S3();  
+
+let createBucket =  (awsS3) => {
+    // Create a bucket and upload something into it
+    let bucketName = awsS3.bucketName || 'node-sdk-00-' + uuid.v4();
+    let bucketRegion = awsS3.bucketRegion || _REGION ;
+    let params = {
+        Bucket : bucketName,
+        CreateBucketConfiguration: {
+            LocationConstraint: bucketRegion
+        }
+    };
+    let returndata = ''
+    console.log(`******************** Creating Bucket ********************`);
+    console.log(`Bucket : ${bucketName} `);
+    console.log(`Region : ${bucketRegion} `);
+    console.log(`*********************************************************`);
+    
+    s3.createBucket(params, function (err, data) {
+        if (err) 
+            console.log(err, err.stack); // an error occurred
+        else 
+            console.log(`Access Location : ${data.Location} `);           // successful response
+        
+            // var params = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' };
+        // s3.putObject(params, function (err, data) {
+        //     if (err)
+        //         console.log(err)
+        //     else
+        //         console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+        // });
+
+    });
+}
+
+let deleteBucket = () => {
+    return 'deleteBucket' ;
+}
+
+let listBuckets = () => {
+    s3.listBuckets((err, data) => {
+        if (err)
+            console.log(err, err.stack); // an error occurred
+        else    {
+            let element = '';
+            odata = data.Buckets
+            odata.forEach(oBucket => {
+                if (oBucket.hasOwnProperty('Name')) {
+                    element += oBucket.Name + '\n';
+                }
+            }); 
+            console.log(element);
+        }
+    }) ;
+}
+
+let deleteOBucket = (awsS3) => {
+    let bucketName = awsS3.bucketName || '';
+    if (bucketName === '') {
+        return console.log('Specify the bucket name.');        
+    }
+
+    let params = {
+        Bucket: bucketName,
+    };
+    let returndata = ''
+    console.log(`******************** Deleting Bucket ********************`);
+    console.log(`Bucket : ${bucketName} `);
+    console.log(`*********************************************************`);
+    
+    s3.deleteBucket(params, (err, data) => {
+        if (err)
+            console.log(err, err.stack); // an error occurred
+        else
+            console.log(`Bucket deleted successfully. `);           // successful response
+    }) ;
+
+}
+
+module.exports = {
+    createBucket,
+    deleteOBucket,
+    listBuckets
+}
